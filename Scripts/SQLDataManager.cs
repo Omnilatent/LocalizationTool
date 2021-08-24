e3536016a0feaec271dffb9d764d54b61f20fca4
nguyentrong101094 <nguyentrong101094@gmail.com>
2021-08-24 15:10:37 +0700

    check database file exist in streaming asset

--- Scripts/SQLDataManager.cs
+++ Scripts/SQLDataManager.cs
@@ -2,6 +2,8 @@
 using System;
 using System.Collections;
 using System.Collections.Generic;
+using System.IO;
+using System.Threading.Tasks;
 using UnityEngine;
 
 namespace Omnilatent.LocalizationTool
@@ -84,25 +86,41 @@ namespace Omnilatent.LocalizationTool
 
         public DataService(string DatabaseName)
         {
+            InitConnection(DatabaseName);
+        }
+
+        async void InitConnection(string DatabaseName)
+        {
             string currentDatabaseVersion = PlayerPrefs.GetString(PREF_EXIST_DATABASE_VERSION_CODE, "");
             bool requireUpdateDatabase = !currentDatabaseVersion.Equals(Application.version);
             Debug.Log($"database:{currentDatabaseVersion} app:{Application.version} update:{requireUpdateDatabase}");
 #if UNITY_EDITOR
             var dbPath = string.Format(@"Assets/StreamingAssets/{0}", DatabaseName);
+            string filePath = Path.Combine(Application.streamingAssetsPath, DatabaseName);
+            if (!File.Exists(filePath))
+            {
+                Debug.LogError($"Please create default database first by Tools/Omnilatent/Localization Tool/Import Extra Package. \n\nDatabase 'localize.db' does not exist in {filePath}.");
+                return;
+            }
 #else
-        // check if file exists in Application.persistentDataPath
-        var filepath = string.Format("{0}/{1}", Application.persistentDataPath, DatabaseName);
+            // check if file exists in Application.persistentDataPath
+            var filepath = string.Format("{0}/{1}", Application.persistentDataPath, DatabaseName);
 
-        if (requireUpdateDatabase || !File.Exists(filepath))
-        {
-            Debug.Log("Database not in Persistent path");
-            // if it doesn't ->
-            // open StreamingAssets directory and load the db ->
+            if (requireUpdateDatabase || !File.Exists(filepath))
+            {
+                Debug.Log("Database not in Persistent path");
+                // if it doesn't ->
+                // open StreamingAssets directory and load the db ->
 #if UNITY_ANDROID
-            var loadDb = new WWW("jar:file://" + Application.dataPath + "!/assets/" + DatabaseName);  // this is the path to your StreamingAssets in android
-            while (!loadDb.isDone) { }  // CAREFUL here, for safety reasons you shouldn't let this while loop unattended, place a timer and error check
-            // then save to Application.persistentDataPath
-            File.WriteAllBytes(filepath, loadDb.bytes);
+                var loadDb = new WWW("jar:file://" + Application.dataPath + "!/assets/" + DatabaseName);  // this is the path to your StreamingAssets in android
+                float timeOut = 10f;
+                while (!loadDb.isDone && timeOut > 0f)
+                {
+                    await Task.Delay(50);
+                    timeOut -= 0.05f;
+                }  // CAREFUL here, for safety reasons you shouldn't let this while loop unattended, place a timer and error check
+                   // then save to Application.persistentDataPath
+                File.WriteAllBytes(filepath, loadDb.bytes);
 #elif UNITY_IOS
                  var loadDb = Application.dataPath + "/Raw/" + DatabaseName;  // this is the path to your StreamingAssets in iOS
                 // then save to Application.persistentDataPath
@@ -127,16 +145,15 @@ namespace Omnilatent.LocalizationTool
 	File.Copy(loadDb, filepath, true);
 
 #endif
-            PlayerPrefs.SetString(PREF_EXIST_DATABASE_VERSION_CODE, Application.version);
-            PlayerPrefs.Save();
-            Debug.Log("Database written");
-        }
+                PlayerPrefs.SetString(PREF_EXIST_DATABASE_VERSION_CODE, Application.version);
+                PlayerPrefs.Save();
+                Debug.Log("Database written");
+            }
 
-        var dbPath = filepath;
+            var dbPath = filepath;
 #endif
-            _connection = new SQLiteConnection(dbPath, SQLiteOpenFlags.ReadWrite | SQLiteOpenFlags.Create);
+            _connection = new SQLiteConnection(dbPath, SQLiteOpenFlags.ReadWrite | SQLiteOpenFlags.Create, true);
             Debug.Log("Final PATH: " + dbPath);
-
         }
 
         public void UpdateData(object obj)
