030b1cbde5f135a7fe25d6c82adb67226df046da
nguyentrong101094 <nguyentrong101094@gmail.com>
2021-08-24 11:19:11 +0700

    old scripts

---
+++ Scripts/LocalizedTMproDirect.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class LocalizedTMproDirect : MonoBehaviour
+{
+    //[SerializeField] string id;
+    [SerializeField] bool hasParam;
+    [SerializeField] string[] paramString;
+
+    string localizeKey;
+    TMP_Text m_Text;
+
+    void Awake()
+    {
+        if (m_Text == null)
+        {
+            m_Text = GetComponent<TMP_Text>();
+        }
+        localizeKey = m_Text.text;
+        UpdateText();
+    }
+
+    public void UpdateText()
+    {
+        //m_Text.text = (!hasParam) ? Localizes.GetString(localizeKey) : string.Format(Localizes.GetString(localizeKey), param);
+        m_Text.text = (!hasParam) ? LocalizeSQLData.GetString(localizeKey) : string.Format(LocalizeSQLData.GetString(localizeKey), paramString);
+    }
+}
