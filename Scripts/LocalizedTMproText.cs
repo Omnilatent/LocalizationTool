06f8cea39210c0e0538539c9df2749c54b14034e
Trong Nguyen <nguyentrong101094@gmail.com>
2024-08-09 17:55:30 +0700

    [WIP] change LocalizedTMproText to make it more customizable and adapt font by searching all fonts for font that support all the character.

--- Scripts/LocalizedTMproText.cs
+++ Scripts/LocalizedTMproText.cs
@@ -10,18 +10,93 @@ namespace Omnilatent.LocalizationTool
         [SerializeField] string id;
         [SerializeField] bool hasParam;
         [SerializeField] int param;
+        [Tooltip("If true, search for supported font on active. If false, use font set by game locale setting")]
+        [SerializeField] private bool _adaptiveFont;
+
+        [SerializeField] private bool _loadTextOnEnable = true;
 
         TMP_Text m_Text;
 
-        void OnEnable()
+        public TMP_Text TmpText
         {
-            if (m_Text == null)
+            get => m_Text;
+            set => m_Text = value;
+        }
+
+        protected virtual void OnEnable()
+        {
+            if (TmpText == null)
             {
-                m_Text = GetComponent<TMP_Text>();
+                TmpText = GetComponent<TMP_Text>();
             }
+
+            if (_loadTextOnEnable)
+            {
+                UpdateText();
+            }
+        }
+
+        public virtual void SetText(string key)
+        {
+            id = key;
+            UpdateText();
+        }
+
+        public virtual void UpdateText()
+        {
             //m_Text.text = (!hasParam) ? Localizes.GetString(id) : string.Format(Localizes.GetString(id), param);
-            m_Text.text = (!hasParam) ? LocalizationController.GetString(id) : string.Format(LocalizationController.GetString(id), param); //Use SQL
-            m_Text.SetCustomFont();
+            
+            TmpText.text = (!hasParam)
+                ? LocalizationController.GetString(id)
+                : string.Format(LocalizationController.GetString(id), param); //Use SQL
+            UpdateFont();
+        }
+
+        public virtual void UpdateFont()
+        {
+            if (!_adaptiveFont)
+            {
+                TmpText.SetCustomFont();
+            }
+            else
+            {
+                CheckSupportedFont(TmpText.text);
+            }
+        }
+
+        protected void CheckSupportedFont(string text)
+        {
+            TMP_FontAsset currentFont = TmpText.font;
+
+            // Check if the current font supports all characters
+            if (!CanDisplayAllCharacters(currentFont, text))
+            {
+                // Iterate through fallback fonts
+                foreach (var fontData in LT_Setting.Instance.fontDatas)
+                {
+                    if (CanDisplayAllCharacters(fontData.fontAsset, text))
+                    {
+                        TmpText.font = fontData.fontAsset;
+                        return;
+                    }
+                }
+            }
+            
+            //failed to find supported font
+            TmpText.SetCustomFont();
+        }
+
+        protected bool CanDisplayAllCharacters(TMP_FontAsset font, string text)
+        {
+            foreach (char c in text)
+            {
+                if (!font.HasCharacter(c))
+                {
+                    return false;
+                }
+            }
+
+            return true;
         }
     }
 }
